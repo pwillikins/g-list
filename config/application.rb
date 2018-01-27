213bc10f8fcfe75da2453704c3d7356a49b7a936
Paul Stone <pmstone247@gmail.com>
2018-01-27 13:02:11 -0700

    config updates for deployment

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module GList
     config.to_prepare do
       DeviseController.respond_to :html, :json
     end
+
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+
+    config.assets.precompile.shift
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
