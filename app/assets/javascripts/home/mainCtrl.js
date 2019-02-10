c77be4d24bf62c5136edd8ae3aa7f7c0d0fcfe41
Paul Stone <pmstone247@gmail.com>
2019-02-10 10:04:25 -0700

    adding dialog to create recipe; adding checkboxes to recipe tiles for future use

--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -1,6 +1,8 @@
 angular.module('g-list')
-.controller('MainCtrl', ['$scope', 'categories', 'Auth', function($scope, categories, Auth){
-  $scope.recipes = categories.recipes;
+  .controller('MainCtrl', [ '$scope', 'categories', '$mdDialog', function ($scope, categories, $mdDialog){
+  $scope.recipes = categories.recipes
+  $scope.recipes.forEach(recipe => recipe.selected = false)
+  $scope.selectedRecipes = []
   
   $scope.createRecipe = function () {
     if ($scope.recipeName == '') { return; }
@@ -14,7 +16,55 @@ angular.module('g-list')
   };
 
   $scope.navigateToRecipe = function (id) {
-    window.location = `#!/recipes/${id}`;
+    window.location = `#!/recipes/${ id }`;
+  }
+
+  $scope.selectRecipe = function (selectedRecipe) {
+    selectedRecipe.selected = !selectedRecipe.selected
+  }
+
+  $scope.areRecipesSelected = function() {
+    let show = false
+    show = $scope.recipes.some(recipe => {
+      return recipe.selected
+    })
+    return show
+  }
+
+  $scope.openNewRecipeDialog = function (ev) {
+    // Appending dialog to document.body to cover sidenav in docs app
+    // Modal dialogs should fully cover application
+    // to prevent interaction outside of dialog
+    $mdDialog.show({
+        controller: DialogController,
+        templateUrl: 'home/_newRecipeDialog.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose: true
+      })
+      .then(function (answer) {
+        $scope.status = 'You said the information was "' + answer + '".';
+      }, function () {
+        $scope.status = 'You cancelled the dialog.';
+      });
+  };
+
+  function DialogController($scope, $mdDialog) {
+    $scope.createRecipe = function () {
+      if ($scope.recipeName == '') { return; }
+      categories.create({
+        name: $scope.recipeName,
+        recipe: true
+      }).then(function (newRecipe) {
+        $scope.navigateToRecipe(newRecipe.id);
+        $mdDialog.hide()
+      });
+      $scope.recipeName = '';
+    };
+
+    $scope.navigateToRecipe = function (id) {
+      window.location = `#!/recipes/${ id }`;
+    }
   }
 
 }]);
