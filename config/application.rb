fa1fe520a2b180a57709cdde99b4abfc6d540fca
Paul Stone <pmstone247@gmail.com>
2019-02-11 08:16:33 -0700

    disabling vulnerability until fix is in place

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module GList
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.compile = false # Disables security vulnerability
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
