824720a135fdcc58698e9c0fcd6b70a80b0218d4
Paul Stone <pmstone247@gmail.com>
2019-11-16 17:54:42 -0600

    wip - initial setup for seeding product data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# products_to_create = [
+#   'Kosher Salt', 'Sea Salt', 'Salt', 'Black Pepper', 'Garlic Powder', 'Garlic Salt', 'Onion Powder',
+#   'Butter', 'Margarine', 'Olive Oil', 'Ketchup', 'Mustard', 'Mayonnaise',
+#   'Ranch Dressing', 'Italian Dressing', 'Vinaigrette Dressing',
+#   'Cheese', 'Cheese Slices', 'Shredded Cheese',
+#   'Ground Beef', 'Steak', 'Chicken', 'Pork', 'Turkey',
+#   ''
+# ]
+# Product.create
